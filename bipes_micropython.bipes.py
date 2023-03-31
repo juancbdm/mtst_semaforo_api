e0088a0820073d887ba0eb8e8a60ac58ebf79c02
Juan Camargo <juancbdm@gmail.com>
2023-03-31 08:47:11 -0300

    fix api

---
+++ bipes_micropython.bipes.py
@@ -0,0 +1,61 @@
+
+import network
+import urequests as requests
+import ujson
+import time
+from machine import Pin
+
+def gpio_set(pin,value):
+    if value >= 1:
+        Pin(pin, Pin.OUT).on()
+    else:
+        Pin(pin, Pin.OUT).off()
+
+def wifi_conn(ssid,passwd):
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    sta_if.active(True)
+    ap_if.active(False)
+    sta_if.connect(ssid, passwd)
+    if sta_if.isconnected() == True:
+        print("Conectado! :)")
+
+wifi_conn("SABAZERA_EXT","00112233445566778899")
+
+while True:
+    res = requests.get('http://ec2-3-237-106-251.compute-1.amazonaws.com/')
+    parsed = ujson.loads(""+res.text+"")
+    parseds = ujson.loads(""+parsed+"")
+    parsedVal = parseds["valueS"]
+
+    if(res.status_code == 200):
+        if parsedVal == '0':
+            print('0')
+            gpio_set((0), False)
+            gpio_set((4), False)
+            gpio_set((5), True) # vermelho 
+        elif parsedVal == '1':
+            print('1')
+            gpio_set((0), True) # verde
+            gpio_set((4), False)
+            gpio_set((5), False)
+        elif parsedVal == '2':
+            print('2')
+            gpio_set((0), False)
+            gpio_set((4), True) # amarelo
+            gpio_set((5), False)
+        else:
+            print('else')
+            gpio_set((0), True)
+            gpio_set((4), True)
+            gpio_set((5), True)
+
+        time.sleep(1)
+        
+    else:
+        res.close()
+        break;
+        
+    
+
+
--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const port = 80;
-var valorSinalLatest = 0;
+var valorSinalLatest = {"valueS": "1"};
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text({type:"*/*"}));
+app.use(bodyParser.json());
 app.post('/', (req, res) => {
-    console.log(req.body);
-    valorSinalLatest = req.body;
+    valorSinalLatest =  Object.keys(req.body)[0];
     res.send(`${valorSinalLatest}`);
 });
 app.get('/', (req, res) => {
